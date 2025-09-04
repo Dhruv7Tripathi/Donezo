82583c3cc1a16f4c472d1d0a4ab278aa617db513
Dhruv7Tripathi <tripathidhruv366@gmail.com>
2025-09-04 23:45:48 +0530

    refactor: enhance CTA and Feature components with improved layout and styles, add FrameworkAgnostic and GlassCard components

--- components/(secondary)/cta.tsx
+++ components/(secondary)/cta.tsx
@@ -3,21 +3,24 @@ import Link from 'next/link'
 
 export default function CTA() {
   return (
-    <div className="bg-black flex flex-col justify-center items-center min-h-screen space-y-4 p-6 rounded-lg shadow-lg">
-      <h2 className="bg-gradient-stop mx-auto max-w-6xl text-balance bg-gradient-to-br from-neutral-800 via-neutral-800 to-neutral-900/30 dark:from-neutral-100 dark:via-neutral-100 via-50% dark:to-neutral-100/30 bg-clip-text py-2 px-2 text-6xl font-bold leading-[1.1] tracking-tighter text-transparent md:text-6xl lg:text-7xl">
-        Ready to get started?
-      </h2>
-      <p className="mt-2 mb-8 max-w-xl text-center text-white text-base">
-        Join thousands of users who&apos;ve already boosted their productivity with our tools.
-        Whether you're managing tasks or planning big projects — we&apos;ve got you covered.
-        Sign up now and take control of your time.
-      </p>
-      <Link
-        href={"/createTodo"}
-        className="group w-40 flex items-center justify-center gap-2 rounded-xl border border-neutral-600 bg-neutral-950 px-5 py-3 text-sm font-semibold text-white shadow-[inset_0px_0px_7px_1px_#535353] transition-all duration-300 hover:bg-neutral-800/50 md:text-[1rem]"
-      >
-        <TextGlitch text={"Get Started"} />
-      </Link>
+    <div className="bg-black flex flex-col justify-center  items-center min-h-screen space-y-4 p-6 rounded-lg shadow-lg">
+      <div className='text-center justify-center w-full rounded-xl p-12  border border-dashed border-neutral-800 flex flex-col items-center  mx-auto'>
+
+        <h2 className="bg-gradient-stop mx-auto max-w-6xl text-balance bg-gradient-to-br from-neutral-800 via-neutral-800 to-neutral-900/30 dark:from-neutral-100 dark:via-neutral-100 via-50% dark:to-neutral-100/30 bg-clip-text py-2 px-2 text-6xl font-bold leading-[1.1] tracking-tighter text-transparent md:text-6xl lg:text-7xl">
+          Ready to get started?
+        </h2>
+        <p className="mt-2 mb-8 max-w-xl text-center text-white text-base">
+          Join thousands of users who&apos;ve already boosted their productivity with our tools.
+          Whether you're managing tasks or planning big projects — we&apos;ve got you covered.
+          Sign up now and take control of your time.
+        </p>
+        <Link
+          href={"/createTodo"}
+          className="group w-40 flex items-center justify-center gap-2 rounded-xl border border-neutral-600 bg-neutral-950 px-5 py-3 text-sm font-semibold text-white shadow-[inset_0px_0px_7px_1px_#535353] transition-all duration-300 hover:bg-neutral-800/50 md:text-[1rem]"
+        >
+          <TextGlitch text={"Get Started"} />
+        </Link>
+      </div>
     </div>
   )
 }
--- components/(secondary)/feature-section-with-bento-grid.tsx
+++ components/(secondary)/feature-section-with-bento-grid.tsx
@@ -1,10 +1,13 @@
 import { User } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import TodoList from "@/components/(secondary)/todo-items";
+import FrameworkAgnostic from '@/components/framework-agnostic';
+import GlassCard from "../glass-card";
+
 export default function Feature() {
   return (
     <div className="w-full py-20 lg:py-40">
-      <div className="container mx-auto">
+      <div className="container max-w-7xl mx-auto">
         <div className="flex flex-col gap-10">
           <div className="flex gap-4 flex-col items-start">
             <div>
@@ -20,29 +23,25 @@ export default function Feature() {
             </div>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            <div className="bg-muted rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
+            <div className="bg-neutral-950 border border-neutral-800 rounded-xl h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
+              <div className="flex flex-col">
+                <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
+
+              </div>
+              <FrameworkAgnostic />
+            </div>
+            <div className="bg-neutral-950 border border-neutral-800 rounded-xl aspect-square p-6 flex justify-between flex-col">
               <User className="w-8 h-8 stroke-1" />
               <TodoList />
               <div className="flex flex-col">
-                <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
+                <h3 className="text-xl tracking-tight">Donezo: Your Task Companion</h3>
                 <p className="text-muted-foreground max-w-xs text-base">
-                  Our goal is to streamline SMB trade, making it easier and faster
-                  than ever.
-                </p>
-              </div>
-            </div>
-            <div className="bg-muted rounded-md  aspect-square p-6 flex justify-between flex-col">
-              <User className="w-8 h-8 stroke-1" />
-              <div className="flex flex-col">
-                <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-                <p className="text-muted-foreground max-w-xs text-base">
-                  Our goal is to streamline SMB trade, making it easier and faster
-                  than ever.
+                  Donezo helps you organize, track, and complete your daily tasks efficiently, so you can focus on what matters most.
                 </p>
               </div>
             </div>
 
-            <div className="bg-muted rounded-md aspect-square p-6 flex justify-between flex-col">
+            <div className="bg-neutral-950 border border-neutral-800 rounded-xl aspect-square p-6 flex justify-between flex-col">
               <User className="w-8 h-8 stroke-1" />
               <div className="flex flex-col">
                 <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
@@ -52,15 +51,17 @@ export default function Feature() {
                 </p>
               </div>
             </div>
-            <div className="bg-muted rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
-              <User className="w-8 h-8 stroke-1" />
-              <div className="flex flex-col">
-                <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
+            <div className="bg-neutral-950 border border-neutral-800 rounded-xl h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
+              <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
+              <div className="flex h-[300px] w-full items-center justify-center bg-zinc-100 p-10 dark:bg-neutral-950">
+                <GlassCard />
+              </div>
+              {/* <div className="flex flex-col">
                 <p className="text-muted-foreground max-w-xs text-base">
                   Our goal is to streamline SMB trade, making it easier and faster
                   than ever.
                 </p>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
