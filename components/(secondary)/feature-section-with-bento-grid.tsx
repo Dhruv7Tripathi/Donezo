b62ff599e6487d287274634c15203f6429f1b200
Dhruv7Tripathi <tripathidhruv366@gmail.com>
2025-09-05 10:38:25 +0530

    feat: add AnimatedBeam component for dynamic SVG path animation between two elements

--- components/(secondary)/feature-section-with-bento-grid.tsx
+++ components/(secondary)/feature-section-with-bento-grid.tsx
@@ -3,19 +3,19 @@ import { Badge } from "@/components/ui/badge";
 import TodoList from "@/components/(secondary)/todo-items";
 import FrameworkAgnostic from '@/components/framework-agnostic';
 import GlassCard from "../glass-card";
-
+import { Beams } from "./beams";
 export default function Feature() {
   return (
-    <div className="w-full py-20 lg:py-40">
+    <div className="w-full py-20 lg:py-20">
       <div className="container max-w-7xl mx-auto">
         <div className="flex flex-col gap-10">
-          <div className="flex gap-4 flex-col items-start">
+          <div className="flex justify-center items-center gap-4 flex-col">
             <div>
               <Badge>Platform</Badge>
             </div>
             <div className="flex gap-2 flex-col">
               <h2 className="text-3xl md:text-5xl tracking-tighter max-w-xl font-regular text-left">
-                Something new!
+                Try Something New
               </h2>
               <p className="text-lg max-w-xl lg:max-w-lg leading-relaxed tracking-tight text-muted-foreground  text-left">
                 Managing a small business today is already tough.
@@ -25,16 +25,16 @@ export default function Feature() {
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             <div className="bg-neutral-950 border border-neutral-800 rounded-xl h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
               <div className="flex flex-col">
-                <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
+                <h3 className="text-xl tracking-tight">Donezo: Simplifying Task Management</h3>
 
               </div>
               <FrameworkAgnostic />
+
             </div>
             <div className="bg-neutral-950 border border-neutral-800 rounded-xl aspect-square p-6 flex justify-between flex-col">
-              <User className="w-8 h-8 stroke-1" />
+              <h3 className="text-xl tracking-tight">Donezo: Your Task Companion</h3>
               <TodoList />
               <div className="flex flex-col">
-                <h3 className="text-xl tracking-tight">Donezo: Your Task Companion</h3>
                 <p className="text-muted-foreground max-w-xs text-base">
                   Donezo helps you organize, track, and complete your daily tasks efficiently, so you can focus on what matters most.
                 </p>
@@ -43,19 +43,20 @@ export default function Feature() {
 
             <div className="bg-neutral-950 border border-neutral-800 rounded-xl aspect-square p-6 flex justify-between flex-col">
               <User className="w-8 h-8 stroke-1" />
+              <div className="flex h-[300px] w-full items-center justify-center bg-zinc-100 p-10 dark:bg-neutral-950">
+                <GlassCard />
+              </div>
               <div className="flex flex-col">
-                <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-                <p className="text-muted-foreground max-w-xs text-base">
+                {/* <h3 className="text-xl tracking-tight">Pay supplier invoices</h3> */}
+                {/* <p className="text-muted-foreground max-w-xs text-base">
                   Our goal is to streamline SMB trade, making it easier and faster
                   than ever.
-                </p>
+                </p> */}
               </div>
             </div>
             <div className="bg-neutral-950 border border-neutral-800 rounded-xl h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
               <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-              <div className="flex h-[300px] w-full items-center justify-center bg-zinc-100 p-10 dark:bg-neutral-950">
-                <GlassCard />
-              </div>
+              <Beams />
               {/* <div className="flex flex-col">
                 <p className="text-muted-foreground max-w-xs text-base">
                   Our goal is to streamline SMB trade, making it easier and faster
